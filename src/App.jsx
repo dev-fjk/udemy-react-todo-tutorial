095cb3d7b6cb3eeb544206ed4648a4a1f8f76402
ふじ <fjk.develop@gmail.com>
2022-05-30 22:25:11 +0900

    追加機能実装前まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import "./style.css";
 
-const App = () => {
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodo] = useState(["あああ", "いいいい"]);
+  const [completeTodos, setCompleteTodo] = useState(["ううう"]);
+
   return (
     <>
-      <h1>Hello World！</h1>
+      <div className="input-area">
+        <input placeholder="TODOを入力" type="text" name="" id="" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <>
+                <div key={todo} className="list-row">
+                  <li>{todo}</li>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <>
+                <div key={todo} className="list-row">
+                  <li>{todo}</li>
+                  <button>戻す</button>
+                </div>
+              </>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
-
-export default App;
